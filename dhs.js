d0263857a4f10c1833ea5cf4a2f1426411c4f737
Pablo Padilla <pgpb.padilla@gmail.com>
2014-02-24 23:01:48 -0800

    Serving files with NodeJS as base64 strings

--- dhs.js
+++ dhs.js
@@ -1,7 +1,26 @@
-var http = require("http");
+// var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(8888);
+
+var fs = require('fs');
+
+var fileContent= fs.readFileSync('pledge.mp3');
+var binaryBuffer= new Buffer(fileContent, 'binary');
+var base64MP3= binaryBuffer.toString('base64');
+
+// console.log("The file is:" + base64MP3);
+
+var http = require('http');
+var server = http.createServer(); 
+var handleReq = function(req,res){
+	res.writeHead(200, {});
+	console.log("Trying to write base64 as response...");
+	res.end(base64MP3); 
+};
+
+server.on('request', handleReq); 
+server.listen(8888);
